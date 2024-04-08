4de80f1140063c2f077b21d26ca4ae3606393634
Ripardo <roope.ripatti@gmail.com>
2024-04-08 17:46:42 +0300

    Implemented header

--- app/r/layout.tsx
+++ app/r/layout.tsx
@@ -1,21 +1,50 @@
 import DashboardNav from "@/components/DashboardNav";
 import Footer from "@/components/footer";
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+
+
 
 export default function DashboardLayout({
     children,
   }: {
     children: React.ReactNode
   }) {
+
+    const supabase = createClient();
+  
+    const signOut = async () => {
+      "use server";
+  
+      const supabase = createClient();
+      await supabase.auth.signOut();
+      return redirect("/");
+    };
+
     return (
         <section className="flex flex-row w-dvw bg-zinc-900 text-white">
             <section className="self-start inset-y-0 left-0 min-w-max w-[350px] h-dvh max-h-screen border-r border-red-300">
                 <DashboardNav />
             </section>
             <div className="w-full flex flex-col items-center">
-                <header className="grow-[1]">
-
+                <header className="w-full p-6 grow-[1] flex flex-row min-h-[100px] max-h-[30vh] justify-around items-center">
+                    <div><h1>Hi there!</h1></div>
+                    <div className="h-full w-[50%] relative">
+                        <Image 
+                            src="/IgniteMainWordmark.svg"
+                            alt="logo"
+                            fill={true}
+                        />
+                    </div>
+                    <form action={signOut}>
+                        <Button variant={"default"}>
+                            Logout
+                        </Button>
+                    </form>
                 </header>
-                <main className="grow-[10]">
+                <main className="grow-[10] h-full w-full">
                     {children}
                     <Footer/>
                 </main>
