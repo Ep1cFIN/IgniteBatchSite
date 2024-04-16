db559a63851a06052cb890e548c9cd1f7b57925d
Ripardo <roope.ripatti@gmail.com>
2024-04-16 15:16:46 +0300

    Purged forgotten tutorial files

--- app/r/profile/page.tsx
+++ app/r/profile/page.tsx
@@ -1,5 +1,19 @@
+import { createClient } from "@/utils/supabase/server";
+
+const name = async () => {
+    const supabase = createClient();
+    
+    const {
+        data: { user },
+    } = await supabase.auth.getUser();
+    
+    return user?.identities?.[0]?.identity_data?.full_name || user?.email || "Unknown";
+    
+}
 
 const HelloWorld= () => {
+    console.log("Wait WTF");
+    name().then((res: any) => console.log(res));
     return <div>Hello World! (profile)</div>;
 };
 
