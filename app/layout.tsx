189a8ec07a5b0e3df746fad5f7e3692d6450f670
Ripardo <roope.ripatti@gmail.com>
2024-04-18 21:23:34 +0300

    refactored dashboardnav and connected it to supabase. Changed the page title.

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Ignite Participant Portal",
+  description: "The one hub for all things Ignite.",
 };
 
 const montserrat = Montserrat({
--- components/DashboardNav.tsx
+++ components/DashboardNav.tsx
@@ -1,69 +1,80 @@
 import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { 
-    DashboardIcon,
-    AvatarIcon,
-    GearIcon,
-    RocketIcon,
-    HeartIcon,
-    PersonIcon,
+import { buttonVariants } from "@/components/ui/button";
+import {
+  DashboardIcon,
+  AvatarIcon,
+  GearIcon,
+  RocketIcon,
+  HeartIcon,
+  PersonIcon,
 } from "@radix-ui/react-icons";
+import AvatarBar from "./AatarBar";
 
-const DashboardNav = () => {
-    return (
-        <section className="p-8 h-full flex flex-col">
-            <div className="pb-12 flex flex-row items-center">
-                <Avatar className="h-12 w-12">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div className="ml-4">
-                    <h2 className="text-lg font-semibold">Roope Ripatti</h2>
-                    <p className="text-sm"> Ignite coach </p>
-                </div>
-                
-            </div>
-            <nav className="flex justify-between flex-col h-full">
-                <ul>
-                    <li className="p-2">MENU</li>
-                    <li className="p-1">
-                        <Link className={buttonVariants({variant: "link"})} href="/r">
-                            <DashboardIcon className="mr-3 h-5 w-5"/>Dashboard
-                        </Link>
-                    </li>
-                    <li className="p-1">
-                        <Link className={buttonVariants({variant: "link"})} href="/r/coaches">
-                            <PersonIcon className="mr-3 h-5 w-5"/>Coaches
-                        </Link>
-                    </li>
-                    <li className="p-1">
-                        <Link className={buttonVariants({variant: "link"})} href="/r/deals">
-                            <HeartIcon className="mr-3 h-5 w-5"/>Deals
-                        </Link>
-                    </li>
-                    <li className="p-1">
-                        <Link className={buttonVariants({variant: "link"})} href="/r/teams">
-                            <RocketIcon className="mr-3 h-5 w-5"/>Teams
-                        </Link>
-                    </li>
-                </ul>
-                <ul>
-                    <li className="p-2 pt-6">SETTINGS</li>
-                    <li className="p-1">
-                        <Link className={buttonVariants({variant: "link"})} href="/r/profile">
-                            <AvatarIcon className="mr-3 h-5 w-5"/>Profile
-                        </Link>
-                    </li>
-                    <li className="p-1">
-                        <Link className={buttonVariants({variant: "link"})} href="/r/settings">
-                            <GearIcon className="mr-3 h-5 w-5"/>Settings
-                        </Link>
-                    </li>
-                </ul>
-            </nav>
-        </section>
-    );
+export const DashboardNav = async () => {
+  return (
+    <section className="p-8 h-full flex flex-col">
+      <AvatarBar />
+      <nav className="flex justify-between flex-col h-full">
+        <ul>
+          <li className="p-2">MENU</li>
+          <li className="p-1">
+            <Link className={buttonVariants({ variant: "link" })} href="/r">
+              <DashboardIcon className="mr-3 h-5 w-5" />
+              Dashboard
+            </Link>
+          </li>
+          <li className="p-1">
+            <Link
+              className={buttonVariants({ variant: "link" })}
+              href="/r/coaches"
+            >
+              <PersonIcon className="mr-3 h-5 w-5" />
+              Coaches
+            </Link>
+          </li>
+          <li className="p-1">
+            <Link
+              className={buttonVariants({ variant: "link" })}
+              href="/r/deals"
+            >
+              <HeartIcon className="mr-3 h-5 w-5" />
+              Deals
+            </Link>
+          </li>
+          <li className="p-1">
+            <Link
+              className={buttonVariants({ variant: "link" })}
+              href="/r/teams"
+            >
+              <RocketIcon className="mr-3 h-5 w-5" />
+              Teams
+            </Link>
+          </li>
+        </ul>
+        <ul>
+          <li className="p-2 pt-6">SETTINGS</li>
+          <li className="p-1">
+            <Link
+              className={buttonVariants({ variant: "link" })}
+              href="/r/profile"
+            >
+              <AvatarIcon className="mr-3 h-5 w-5" />
+              Profile
+            </Link>
+          </li>
+          <li className="p-1">
+            <Link
+              className={buttonVariants({ variant: "link" })}
+              href="/r/settings"
+            >
+              <GearIcon className="mr-3 h-5 w-5" />
+              Settings
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </section>
+  );
 };
 
 export default DashboardNav;
